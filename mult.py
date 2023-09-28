cef91534edf37beaa233942d5ad9b24a578bc0ad
Rafael Terra <rafaelstjf@hotmail.com>
2023-09-28 17:08:11 -0300

    update on automate tutorial draft

--- mult.py
+++ mult.py
@@ -3,6 +3,7 @@ import os, argparse, sys
 import django
 import subprocess as sb
 def main():
+    my_env = os.environ.copy()
     genomes = list()
     parser = argparse.ArgumentParser()
     parser.add_argument('genomes', help="List of Genbank accession numbers for genomes, separated with new lines",
@@ -14,12 +15,16 @@ def main():
         genomes.append(l.strip().upper())
     for genome in genomes:
         folder_name = genome.split('_')[1][:3]
-        path = os.path.join(os.getcwd(),f"data/{folder_name}/{genome}.tar.gz")
-        o = sb.check_output(["python", "manage.py", "download_gbk", f"{genome} | gzip"])
+
+        path = os.path.join(os.getcwd(),f"data/{folder_name}/{genome}/{genome}.gbk.gz")
+        o = sb.check_output(["python", "manage.py", "download_gbk", f"{genome}"], env = my_env)
         print(o)
-        sb.check_output(["python", "manage.py", "load_gbk", f"{path} --overwrite"])
-        sb.check_output(["python", "manage.py", "index_genome_db", f"{genome}"])
-        sb.check_output(["python", "manage.py", "index_genome_seq", f"{genome}"])
+        o = sb.check_output(["python", "manage.py", "load_gbk", f"{path}",  "--overwrite"], env = my_env)
+        print(o)
+        o = sb.check_output(["python", "manage.py", "index_genome_db", f"{genome}"], env = my_env)
+        print(o)
+        o = sb.check_output(["python", "manage.py", "index_genome_seq", f"{genome}"], env = my_env)
+        print(o)
 
 
 
