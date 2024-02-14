294d46859a29e24daf876c13a67de82a4698245f
Gabi <lucasgabrielgarcia1@gmail.com>
2024-02-14 15:18:51 -0300

    Dropdown now fetchs from biodatabase

--- tpweb/views/FormView.py
+++ tpweb/views/FormView.py
@@ -9,16 +9,25 @@ class FormView(View):
     template_name = 'blast/form.html'
 
     def get(self, request, *args, **kwargs):
+        biodatabases = Biodatabase.objects.all()
+        options = [{'value': bd.biodatabase_id, 'label': bd.name} for bd in biodatabases]
 
-        return render(request, self.template_name)
+        # Render the form with context
+        return render(request, self.template_name, {
+            'options': options,
+        })
     
     def post(self, request, *args, **kwargs):
         if request.method == 'POST':
             selected_item = request.POST.get('dropdown')
             text_input = request.POST.get('text_input')
-        # Process form data here (e.g., validate, save to database, etc.)
+        
+            # Based on selected_item, you can access the corresponding Biodatabase object
+            selected_biodatabase = Biodatabase.objects.get(pk=selected_item)
+            # ... perform actions using selected_biodatabase and text_input
+
             return render(request, self.template_name, {
-            'message': f"You selected item {selected_item} and entered text: '{text_input}'.",
-})
+                'message': f"You selected item {selected_biodatabase.name} {selected_biodatabase.description} and entered text: '{text_input}'.",
+            })
         else:
-            return render(request, 'form_template.html', {})
+            return render(request, self.template_name, {})
