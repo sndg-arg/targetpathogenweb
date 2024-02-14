4bc262a0ff03734dc7b50fc27683f53c5fbafc68
Gabi <lucasgabrielgarcia1@gmail.com>
2024-02-14 13:38:29 -0300

    Added dropdown and input query field

--- tpweb/views/FormView.py
+++ tpweb/views/FormView.py
@@ -11,3 +11,14 @@ class FormView(View):
     def get(self, request, *args, **kwargs):
 
         return render(request, self.template_name)
+    
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            selected_item = request.POST.get('dropdown')
+            text_input = request.POST.get('text_input')
+        # Process form data here (e.g., validate, save to database, etc.)
+            return render(request, self.template_name, {
+            'message': f"You selected item {selected_item} and entered text: '{text_input}'.",
+})
+        else:
+            return render(request, 'form_template.html', {})
