554fd479b24a9e58e9042bcb44df5c9f4071ec3f
gabi <lucasgabrielgarcia1@gmail.com>
2024-06-28 16:01:11 -0300

    Added redirection funtionality to protein view with options data.

--- tpweb/views/ParameterForm.py
+++ tpweb/views/ParameterForm.py
@@ -3,11 +3,12 @@ from django import forms
 
 class ParameterForm(forms.Form):
     param = forms.ModelChoiceField(queryset = ScoreParam.objects.all(),
-                                   widget=forms.Select(attrs={"hx-get": "load_options/", "hx-target": "#id_options"}))
+                                   widget=forms.Select(attrs={"hx-get": "../load_options/", "hx-target": "#id_options"}))
     options = forms.ModelChoiceField(queryset = ScoreParamOptions.objects.none())
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        print(self.data)
         if "param" in self.data:
             param_id = int(self.data.get("param"))
             self.fields["options"].queryset = ScoreParamOptions.objects.filter(score_param_id=param_id)
