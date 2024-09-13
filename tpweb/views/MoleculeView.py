cbb17583d790d3e86f71c8fba43fdb051589a8dd
Gabi <lucasgabrielgarcia1@gmail.com>
2024-09-13 16:30:08 -0300

    Basic molecule display
    The SMILES is hardcoded.

---
+++ tpweb/views/MoleculeView.py
@@ -0,0 +1,22 @@
+from django.views.generic import View
+from django.shortcuts import render
+from rdkit import Chem
+from rdkit.Chem.Draw import rdMolDraw2D
+
+class MoleculeView(View):
+    def get(self, request):
+        smiles = 'Cc1ccccc1'
+        try:
+            mol = Chem.MolFromSmiles(smiles)
+            canvas_width_pixels = 300
+            canvas_height_pixels = 300
+            
+            drawer = rdMolDraw2D.MolDraw2DSVG(canvas_width_pixels, canvas_height_pixels)
+            drawer.DrawMolecule(mol)
+            drawer.FinishDrawing()
+            
+            svg_data = drawer.GetDrawingText()
+            
+            return render(request, 'molecule/molecule.html', {'svg': svg_data})
+        except Exception as e:
+            return render(request, 'molecule/error.html', {'message': f"Error rendering molecule: {str(e)}"})
