6f4b0cd3742cffd7855a4ac78753bc4f06d0715e
Gabi <lucasgabrielgarcia1@gmail.com>
2024-09-30 13:56:41 -0300

    Added scripts to fetch binders data

--- tpweb/management/commands/get_compounds.py
+++ tpweb/management/commands/get_compounds.py
@@ -1,4 +1,5 @@
 import os
+import re
 import pandas as pd
 import subprocess as sp
 from bioseq.models.Biodatabase import Biodatabase
@@ -6,6 +7,8 @@ from django.core.management.base import BaseCommand, CommandError
 from bioseq.io.SeqStore import SeqStore
 from bioseq.models.Bioentry import Bioentry
 import gzip
+from Bio.PDB.MMCIF2Dict import MMCIF2Dict
+from pdbecif.mmcif_io import CifFileReader
 
 
 class Command(BaseCommand):
@@ -15,13 +18,12 @@ class Command(BaseCommand):
         parser.add_argument('tpwebdir')
 
     def handle(self, *args, **options):
-
         def create_biolip_dataframe(tpwebdir):
             try:
                 with gzip.open(os.path.join(tpwebdir, 'biolip', 'BioLiP.txt.gz'), 'rt') as biolip_file:
                     data = [line.strip().split('\t') for line in biolip_file]
 
-                custom_headers = ['PDB ID', 'Receptor chain', 'Resolution', 'Binding site number code', 'Ligand ID', 'Ligand chain', 'Ligand serial numbera', 'Binding site residues (with PDB residue numbering)', 'Binding site residues (with residue re-numbered starting from 1)', 'Catalytic site residues (with PDB residue numbering)', 'Catalytic site residues (with residue re-numbered starting from 1)', 'EC number', 'GO terms', 'Binding affinity by manual survey of the original literature', 'Binding affinity provided by the Binding MOAD database', 'Binding affinity provided by the PDBbind-CN database.', 'Binding affinity provided by the BindingDB database', 'UniProt ID', 'PubMed ID', 'Residue sequence number of the ligand', 'Receptor sequence']
+                custom_headers = ['PDB ID', 'Receptor chain', 'Resolution', 'Binding site number code', 'Ligand ID', 'Ligand chain', 'Ligand serial numbera', 'Binding site residues (with PDB residue numbering)', 'Binding site residues (with residue re-numbered starting from 1)', 'Catalytic site residues (with PDB residue numbering)', 'Catalytic site residues (with residue re-numbered starting from 1)', 'EC number', 'GO terms', 'Binding affinity by manual survey of the original literature', 'Binding affinity provided by the Binding MOAD database', 'Binding affinity provided by the PDBbind-CN database.', 'Binding affinity provided by the BindingDB database', 'Uniprot', 'PubMed ID', 'Residue sequence number of the ligand', 'Receptor sequence']
                 df = pd.DataFrame(data, columns=custom_headers)
                 return df
 
@@ -32,11 +34,46 @@ class Command(BaseCommand):
                 print(f"An error occurred: {str(e)}")
                 return None
 
-        def create_locustag_dataframe(tpwebdir):
+        def download_databases(tpwebdir):
+            if not os.path.exists(f'{tpwebdir}/biolip'):
+                print('Biolip folder does not exist, creating one...')
+                os.makedirs(f'{tpwebdir}/biolip')
+                url = "https://zhanggroup.org/BioLiP/download/BioLiP.txt.gz"
+                output_filename = f"{tpwebdir}/biolip/BioLiP.txt.gz"
+                curl_command = ["curl", "-L", "-o", output_filename, url]
+
+                try:
+                    print('Downloading Biolip...')
+                    result = sp.run(curl_command, capture_output=True, text=True)
+                    if result.returncode == 0:
+                        print(f"File '{output_filename}' has been successfully downloaded.")
+                    else:
+                        print(f"Failed to download the file. Return code: {result.returncode}")
+                        print(f"Error message: {result.stderr}")
+                except Exception as e:
+                    print(f"An error occurred while trying to download the file: {str(e)}")
+
+                url = "https://files.wwpdb.org/pub/pdb/data/monomers/components.cif"
+                output_filename = f"{tpwebdir}/biolip/components.cif"
+                curl_command = ["curl", "-L", "-o", output_filename, url]
+
+                try:
+                    print('Downloading Chemical Component Dictionary...')
+                    result = sp.run(curl_command, capture_output=True, text=True)
+                    if result.returncode == 0:
+                        print(f"File '{output_filename}' has been successfully downloaded.")
+                    else:
+                        print(f"Failed to download the file. Return code: {result.returncode}")
+                        print(f"Error message: {result.stderr}")
+                except Exception as e:
+                    print(f"An error occurred while trying to download the file: {str(e)}")
+
+        def create_locustag_dataframe(tpwebdir, folder_path):
             try:
                 with open(os.path.join(folder_path, genome + '_unips.lst'), 'r') as locus_tag:
-                    data = [line.strip().split('\t') for line in locus_tag]
-                df = pd.DataFrame(data)
+                    data = [line.strip().split(' ') for line in locus_tag]
+                custom_headers = ['Uniprot', 'Locustag']
+                df = pd.DataFrame(data, columns=custom_headers)
                 return df
 
             except FileNotFoundError:
@@ -46,39 +83,25 @@ class Command(BaseCommand):
                 print(f"An error occurred: {str(e)}")
                 return None
 
+        def delete_ubiquious(df):
+            ubiquious = ['ZN', 'ATP', 'LEU']
+            for compound in ubiquious:
+                df = df[df['Ligand ID'] != compound]
+            return df
+
         tpwebdir = options['tpwebdir']
         genome = options['genome']
         ss = SeqStore('./data')
         folder_path = ss.db_dir(genome)
 
-        if not os.path.exists(f'{tpwebdir}/biolip'):
-            print('Biolip folder does not exist, creating one...')
-            os.makedirs(f'{tpwebdir}/biolip')
-            url = "https://zhanggroup.org/BioLiP/download/BioLiP.txt.gz"
-            output_filename = f"{tpwebdir}/biolip/BioLiP.txt.gz"
-            curl_command = ["curl", "-L", "-o", output_filename, url]
-
-            try:
-                result = sp.run(curl_command, capture_output=True, text=True)
-                if result.returncode == 0:
-                    print(f"File '{output_filename}' has been successfully downloaded.")
-                else:
-                    print(f"Failed to download the file. Return code: {result.returncode}")
-                    print(f"Error message: {result.stderr}")
-
-            except Exception as e:
-                print(f"An error occurred while trying to download the file: {str(e)}")
-
-        unip_file = open(os.path.join(folder_path, genome + '_unips.lst'), 'r')
-        uniprots = (line.strip().split(' ') for line in unip_file)
-
+        download_databases(tpwebdir)
         biolip = create_biolip_dataframe(tpwebdir)
-        locustag = create_locustag_dataframe(tpwebdir)
+        locustag = create_locustag_dataframe(tpwebdir, folder_path)
+        locustag = locustag.merge(biolip, how='left', on='Uniprot')
+        locustag = locustag.dropna(subset=['Ligand ID'])
+        locustag = delete_ubiquious(locustag)
+        locustag.to_csv('binders.csv', index=False)
 
-        if biolip is not None:
-            print(biolip.head())  # Print the first few rows of the DataFrame
-
-
-
-
-
+        #ccd_cif = os.path.abspath(f"{tpwebdir}/biolip/components.cif")
+        #data = CifFileReader().read(ccd_cif)
+        #print(len(data))
