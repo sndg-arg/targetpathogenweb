f177229e48d67ae89832e00463c085cbd22bb1b5
Ana Gabriela Gutson <agutson@fi.uba.ar>
2026-04-14 20:08:39 -0300

    fix: exports.sh should not override container env vars

--- pipeline/exports.sh
+++ pipeline/exports.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export DJANGO_DEBUG=True
+export DJANGO_DEBUG=${DJANGO_DEBUG:-True}
 export DJANGO_SETTINGS_MODULE=tpwebconfig.settings
-export DJANGO_DATABASE_URL=postgres://postgres:123@db:5432/tp?sslmode=disable
+export DJANGO_DATABASE_URL=${DJANGO_DATABASE_URL:-postgres://postgres:123@db:5432/tp?sslmode=disable}
 export CELERY_BROKER_URL=redis://localhost:6379/0
 # Prepend project root so local command overrides can shadow site-packages when needed.
 export PYTHONPATH=/app/targetpathogenweb:${PYTHONPATH}:../../sndgjobs:../../sndgbiodb:../../targetpathogen:../../sndg-bio:../../targetpathogenweb:../../targetpathogenweb/pipeline
