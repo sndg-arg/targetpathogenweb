72616b98cba3304daf3132b777eac653e5c75e1d
AnaGutson <83771013+AnaGutson@users.noreply.github.com>
2026-06-10 21:26:22 -0300

    i18n: remove LocaleMiddleware, fix Spanish strings in protein_test template

--- tpwebconfig/settings.py
+++ tpwebconfig/settings.py
@@ -81,7 +81,6 @@ MIDDLEWARE = [
     "tpweb.middleware.observability.RequestTimingMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "django.middleware.locale.LocaleMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
